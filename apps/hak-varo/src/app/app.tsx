a1d328b88895a33adfce0ed05d5d288422b6ec15
Behemoth <simonostini@gmail.com>
2021-12-17 17:44:06 +0100

    increased time to register

--- apps/hak-varo/src/app/app.tsx
+++ apps/hak-varo/src/app/app.tsx
@@ -3,7 +3,7 @@ import { Registration } from '@hak-varo/registration';
 export const App = () => {
   return (
     <Registration
-      endDate={new Date(2021, 11, 17)}
+      endDate={new Date(2021, 11, 18)}
       registrationURL="https://forms.gle/y2EwntW7L2V67aDe6"
     />
   );
